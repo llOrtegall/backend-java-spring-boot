8cd2ca63e1523049d66c9ffdb9524f8c55e6e2ca
llortegall <IvanOrtega_97@hotmail.com>
2026-02-20 12:56:55 -0500

    feat: add Gradle wrapper and initial application properties for database configuration

---
+++ src/test/java/com/backend/ortega/OrtegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ortega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrtegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
