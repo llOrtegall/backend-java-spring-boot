054123ee060c0fac781c16db36c6f473afa77d42
llortegall <ivanortega_97@hotmail.com>
2026-02-20 18:20:45 -0500

    refactor: remove Product-related classes and interfaces for code cleanup

--- src/main/java/com/backend/ortega/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.ortega.repositories;
-
-import com.backend.ortega.entities.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product , Long> {
-}
